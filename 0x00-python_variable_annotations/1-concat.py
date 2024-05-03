52ca9efe512be76484b24da866e2c78159c56a15
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-05-04 00:11:07 +0300

    greedy

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This module contains the function concat that takes two strings and returns their concatenation
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Returns the concatenation of two strings
+    '''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+This module contains the function safe_first_element that takes a list of any type and returns the first element of the list if it exists, otherwise None
+'''
+
+
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''
+    Returns the first element of a list
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
