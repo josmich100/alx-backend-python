dff73e093de2c8a3d22dca11bccc0ea1ea04ecc6
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-05-04 00:08:02 +0300

    greed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This module contains the function add that takes two floats and returns their sum
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Returns the sum of two floats
+    '''
+    return a + b
