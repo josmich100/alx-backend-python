faa11cd4c2523c6b0146dacc57cd4300e628fa75
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2024-06-01 21:49:33 +0300

    free

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Utils module
+"""
+import requests
+from functools import wraps
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
+
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """
+    Access nested map
+    """
+    for key in path:
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
+        nested_map = nested_map[key]
+
+    return nested_map
+
+
+def get_json(url: str) -> Dict:
+    """
+    Get json from url
+    """
+    response = requests.get(url)
+    return response.json()
+
+
+def memoize(fn: Callable) -> Callable:
+    """
+    Memoize decorator.
+    """
+    attr_name = "_{}".format(fn.__name__)
+
+    @wraps(fn)
+    def memoized(self):
+        """"memoized wraps"""
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
